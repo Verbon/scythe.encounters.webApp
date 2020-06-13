28d9161e145d15785e324a1b19c94d85bc6fb558
Vladislav Narutsky <uladzislau.narutsky@itechart-group.com>
2020-06-13 12:37:46 +0300

    Fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,7 @@ import App from './App';
 
 test('renders learn react link', () => {
     const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
+    const encountersElement = getByText(/encounters/i);
 
-    expect(linkElement).toBeInTheDocument();
+    expect(encountersElement).toBeInTheDocument();
 });
